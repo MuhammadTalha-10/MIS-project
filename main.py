23a7ffee89c49bdb813a116c7f5df795a1b3cbe8
CyberGai <cyb3rgai@proton.me>
2022-12-21 21:25:41 +0500

    basic fastapi template

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory='templates')
-app.mount("/static", StaticFiles(directory='static'), name='static')
-
 
 @app.get("/")
 async def root():
-    return
-
-
-@app.get("/login")
-def login():
-    return
+    return {"message": "Hello World"}
